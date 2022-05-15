70b4384854d8669a0322092712cb3bfa220bf117
Yruam Käffer de Faria <yruamkf@gmail.com>
2022-05-15 13:40:00 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const linksSocialMedia = {
+  github: 'yruamkaffer',
+  youtube: 'yruamkaffer',
+  facebook: 'yruamkaffer',
+  instagram: 'yruamkaffer',
+  twitter: 'yruamkaffer'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
